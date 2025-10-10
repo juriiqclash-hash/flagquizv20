947fa740ff611b255729f24b338ee4321843ca29
juriiqclash-hash <juriiq.clash@gmail.com>
2025-10-11 01:00:05 +0200

    Updated MainMenu.tsx

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -79,7 +79,7 @@ export default function MainMenu({ onStart, onMultiplayerStart, onDailyChallenge
       }}
     >
       {/* Language Selector - Top Left */}
-      <div className="absolute top-6 left-6 z-20 scale-90">
+      <div className="absolute top-6 left-6 z-20 scale-90 origin-top-left">
         <Select value={language} onValueChange={setLanguage}>
           <SelectTrigger className="w-[160px] bg-white/10 text-white border-white/20 hover:bg-white/20">
             <Languages className="mr-2 h-4 w-4" />
@@ -97,12 +97,12 @@ export default function MainMenu({ onStart, onMultiplayerStart, onDailyChallenge
       </div>
 
       {/* Profile Button - Top Right */}
-      <div className="absolute top-6 right-6 z-20 scale-90">
+      <div className="absolute top-6 right-6 z-20 scale-90 origin-top-right">
         <ProfileButton transparentStyle onProfileOpenChange={() => {}} />
       </div>
 
       {/* Left Column */}
-      <div className="absolute bottom-6 left-6 z-10 flex flex-col gap-5 scale-90" style={{ width: '420px' }}>
+      <div className="absolute bottom-6 left-6 z-10 flex flex-col gap-5 scale-90 origin-bottom-left" style={{ width: '420px' }}>
         {/* News/Discord Panel */}
         <div
           className="bg-white/10 backdrop-blur-sm rounded-3xl p-6 border border-white/20 transition-all duration-300 hover:bg-white/15 cursor-pointer overflow-hidden"
@@ -152,7 +152,7 @@ export default function MainMenu({ onStart, onMultiplayerStart, onDailyChallenge
       </div>
 
       {/* Right Column */}
-      <div className="absolute bottom-6 right-6 z-10 flex flex-col gap-5 scale-90" style={{ width: '420px' }}>
+      <div className="absolute bottom-6 right-6 z-10 flex flex-col gap-5 scale-90 origin-bottom-right" style={{ width: '420px' }}>
         {/* Daily Challenge Panel */}
         <div className="bg-white/10 backdrop-blur-sm rounded-3xl p-5 border border-white/20 transition-all duration-300 hover:bg-white/15">
           <div className="flex items-center justify-between">
