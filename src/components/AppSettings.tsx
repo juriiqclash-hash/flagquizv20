6ca8b1c701ec04d32415e9194afa3513b8c33630
juriiqclash-hash <juriiq.clash@gmail.com>
2025-10-28 22:09:57 +0100

    Updated AppSettings.tsx

--- src/components/AppSettings.tsx
+++ src/components/AppSettings.tsx
@@ -1306,8 +1306,8 @@ const AppSettings = ({ open, onOpenChange }: AppSettingsProps) => {
             </div>
 
             {/* Right Content Area */}
-            <div className="flex-1 overflow-y-auto">
-              <div className="p-6 pb-12">
+            <div className="flex-1 flex flex-col min-h-0">
+              <div className="flex-1 overflow-y-auto p-6 pb-12">
                 <div className="max-w-3xl">
                   {renderContent()}
                 </div>
